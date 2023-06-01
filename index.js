4191ca35c470632f509045f0d35511c696553179
Loken <rodrigomencias08@gmail.com>
2023-05-31 21:03:49 -0600

    update index.js

--- index.js
+++ index.js
@@ -10,65 +10,69 @@ const port = 3001;
 const uri = 'mongodb+srv://rodrigomencias08:o8Nl0JitEmFTB6YB@cluster0.7ipkl3j.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient(uri);
 
-app.use(express.json());
-
-app.get('/users', async (req, res) => {
+async function startServer() {
   try {
     await client.connect();
-    const db = client.db('peliculas');
-    const collection = db.collection('peliculas_comments');
-    const users = await collection.find().toArray();
-    res.json(users);
+    console.log('Conexión a MongoDB establecida');
+
+    app.use(express.json());
+
+    app.get('/users', async (req, res) => {
+      try {
+        const db = client.db('peliculas');
+        const collection = db.collection('peliculas_comments');
+        const users = await collection.find().toArray();
+        res.json(users);
+      } catch (error) {
+        console.error('Error al obtener los usuarios', error);
+        res.status(500).json({ error: 'Error al obtener los usuarios' });
+      }
+    });
+
+    app.get('/documents/:objectId/reactions/:reactionId', async (req, res) => {
+      const objectId = req.params.objectId;
+      const reactionId = req.params.reactionId;
+
+      try {
+        const db = client.db('peliculas');
+        const collection = db.collection('peliculas_summaryreactions');
+
+        const query = { "_id.objectId": objectId, "_id.reactionId": reactionId };
+        const result = await collection.find(query).toArray();
+
+        console.log('Registros encontrados:', result);
+        res.json(result);
+      } catch (error) {
+        console.error('Error al realizar la consulta', error);
+        res.status(500).json({ error: 'Error al realizar la consulta' });
+      }
+    });
+
+    app.get('/commentsByUserId/:userId/', async (req, res) => {
+      const userId = req.params.userId;
+
+      try {
+        const db = client.db('peliculas');
+        const collection = db.collection('peliculas_comments');
+
+        const query = { "objectId": userId };
+        const result = await collection.find(query).toArray();
+
+        console.log('Registros encontrados:', result);
+        res.json(result);
+      } catch (error) {
+        console.error('Error al realizar la consulta', error);
+        res.status(500).json({ error: 'Error al realizar la consulta' });
+      }
+    });
+
+    app.listen(port, () => {
+      console.log(`Servidor escuchando en http://localhost:${port}`);
+    });
   } catch (error) {
-    console.error('Error al obtener los usuarios', error);
-    res.status(500).json({ error: 'Error al obtener los usuarios' });
+    console.error('Error al conectar a MongoDB', error);
+    process.exit(1); // Salir de la aplicación si no se puede conectar a MongoDB
   }
-});
+}
 
-
-app.get('/documents/:objectId/reactions/:reactionId', async (req, res) => {
-  const objectId = req.params.objectId;
-  const reactionId = req.params.reactionId;
-
-  try {
-    await client.connect();
-    const db = client.db('peliculas');
-    const collection = db.collection('peliculas_summaryreactions');
-
-    const query = { "_id.objectId": objectId, "_id.reactionId": reactionId };
-    const result = await collection.find(query).toArray();
-
-    console.log('Registros encontrados:', result);
-    res.json(result);
-  } catch (error) {
-    console.error('Error al realizar la consulta', error);
-    res.status(500).json({ error: 'Error al realizar la consulta' });
-  } finally {
-    await client.close();
-  }
-});
-
-app.get('/commentsByUserId/:userId/', async (req, res) => {
-  const userId = req.params.userId;
-
-  try {
-    await client.connect();
-    const db = client.db('peliculas');
-    const collection = db.collection('peliculas_comments');
-
-    const query = { "userId": userId };
-    const result = await collection.find(query).toArray();
-
-    console.log('Registros encontrados:', result);
-    res.json(result);
-  } catch (error) {
-    console.error('Error al realizar la consulta', error);
-    res.status(500).json({ error: 'Error al realizar la consulta' });
-  } finally {
-    await client.close();
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
-});
+startServer();
